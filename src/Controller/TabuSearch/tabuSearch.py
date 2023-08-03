26f8de4481179a8127f3b3cd4716f2ba4af462c0
George Maia <georgemaiaf@gmail.com>
2023-08-03 08:31:40 -0300

    feat: tabuSearch

--- src/Controller/TabuSearch/tabuSearch.py
+++ src/Controller/TabuSearch/tabuSearch.py
@@ -2,79 +2,58 @@ from random import randint
 from typing import List
 from ...Model.node import Node
 from ...Model.route import Route
-
-
-def tabu_search_vrp(nodes: list[Node], tabu_size: int, max_iterations: int) -> Route:
-    # Initialize a solution with random routes for each vehicle
-    initial_solution = Route()
-
-    depot = nodes.pop(0)
-    initial_solution.setNode(depot, 0)
-
-    len_route = len(nodes)
-
-    while len(nodes):
-        n = randint(0, len(nodes) - 1)
-        node = nodes.pop(n)
-
-        initial_solution.setNode(node, node.distanceTo(depot))
-
-    # Tabu search variables
-    best_solution = initial_solution
-    best_cost = best_solution.getCost()
-    tabu_list = []
-    iteration = 0
-
-    while iteration < max_iterations:
-        candidate_solutions: List[Route] = []
-        candidate_solution = best_solution
-
-        for k in range(1, len_route - 1):
-            for l in range(1, len_route - 1):
-                move = (k, l)
-
-                iteration += 1
-                if move in tabu_list:
-                    iteration += 1
-                    continue
-
-                candidate_solution = doMove(candidate_solution, k, l)
-
-                # Add the move to the tabu list
-                tabu_list.append(move)
-
-                if len(tabu_list) > tabu_size:
-                    tabu_list.pop(0)
-
-                candidate_solutions.append(candidate_solution)
-
-        # Choose the best candidate solution
-        candidate_solutions.sort(key=lambda r: r.getCost())
-        best_candidate = candidate_solutions[0]
-
-        if best_candidate.getCost() < best_cost:
-            best_solution = best_candidate
-            best_cost = best_candidate.getCost()
-            iteration = 0
-
-    return best_solution
-
-
-def doMove(route: Route, i: int, j: int) -> Route:
-    node_aux: Node = route._route[i]
-    route._route[i] = route._route[j]
-    route._route[j] = node_aux
-
-    return route
-
+from ...Model.heuristics import *
 
 class TabuSearch:
     def __init__(self, nodes: List[Node], tabu_list_size: int, max_iterations: int):
         self._tabu_list = []
         self._tabu_list_size = tabu_list_size
         self._max_iterations = max_iterations
+        self._iteration = 0
 
         self.route: Route
 
         # self.RandIns(nodes)
-        # self.NearestNeighbor(nodes)    
+        # self.NearestNeighbor(nodes) 
+
+        return self.tabuSearch()
+
+    def tabuSearch(self) -> Route:
+        while self.isStop():
+            i = randint(1, len(self.route) - 1 - 1) 
+            j = randint(i + 1, len(self.route) - 1 - 1)
+            move = Moves(randint(0, 2))
+            candidate_route: Route 
+
+            self._iteration += 1
+
+            if (i, j, move) in self._tabu_list:
+                continue
+
+            match move:
+                case Moves.SWAP: 
+                    candidate_route = Swap(self.route, i, j)
+                case Moves.TWOOPT: 
+                    candidate_route = TwoOPT(self.route, i, j)
+                case Moves.OROPT:
+                    # candidate_route = OrOPT(self.route, i, j, 1)
+                    # candidate_route = OrOPT(self.route, i, j, 2)
+                    # candidate_route = OrOPT(self.route, i, j, 3)
+                    candidate_route = OrOPT(self.route, i, j, randint(1, j - 1))
+            
+            if candidate_route.getCost() < self.route.getCost():
+                self.route = candidate_route
+                self._iteration = 0
+                continue
+            
+            self._tabu_list.append(i, j, move)
+        
+        return self.route
+    
+    def isStop(self) -> bool:
+        return self._iteration > self._max_iterations
+
+class Moves(Enum):
+    SWAP = 0
+    TWOOPT = 1
+    OROPT = 2
