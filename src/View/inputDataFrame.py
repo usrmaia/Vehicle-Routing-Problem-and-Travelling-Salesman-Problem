9e85a921acfb7f27cdc378411a09acd376e0c63e
George Maia <georgemaiaf@gmail.com>
2023-08-06 02:02:16 -0300

    feat: lower bound

--- src/View/inputDataFrame.py
+++ src/View/inputDataFrame.py
@@ -1,17 +1,23 @@
+from typing import List
 from io import TextIOWrapper
 from Model.node import Node
 
 
 class DataFrame:
-    def __init__(self, path):
+    def __init__(self, path) -> None:
         self.path = path
-        self.nodes: list[Node] = []
+        self.nodes: List[Node] = []
+        self.lowerBound = -1
 
         self._readDataSet()
 
     def _readDataSet(self):
         with open(self.path, "r") as lines:
             for line in lines:
+                if line.startswith("LOWER_BOUND"):
+                    self.lowerBound = float(line.split()[2])
+                    continue
+
                 if not line.startswith("NODE_COORD_SECTION"):
                     continue
 
@@ -28,5 +34,5 @@ class DataFrame:
                 Node(int(node_data[0]), float(node_data[1]), float(node_data[2]))
             )
 
-    def getNodes(self) -> list[Node]:
-        return self.nodes
+    def getDataFrame(self) -> tuple[List[Node], float]:
+        return self.nodes, self.lowerBound
