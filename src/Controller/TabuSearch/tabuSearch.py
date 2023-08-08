bfbff6b7b24cf46881e05bbc21884c91c8626d89
George Maia <georgemaiaf@gmail.com>
2023-08-08 10:19:24 -0300

    feat: busca tabu

--- src/Controller/TabuSearch/tabuSearch.py
+++ src/Controller/TabuSearch/tabuSearch.py
@@ -5,11 +5,12 @@ from ...Model.route import Route
 from ...Model.heuristics import *
 
 class TabuSearch:
-    def __init__(self, nodes: List[Node], tabu_list_size: int, max_iterations: int):
+    def __init__(self, nodes: List[Node], lower_bound: float, tabu_list_size: int, max_iterations: int):
         self._tabu_list = []
         self._tabu_list_size = tabu_list_size
         self._max_iterations = max_iterations
         self._iteration = 0
+        self._lower_bound = lower_bound
 
         self.route: Route
 
@@ -22,37 +23,53 @@ class TabuSearch:
         while self.isStop():
             i = randint(1, len(self.route) - 1 - 1) 
             j = randint(i + 1, len(self.route) - 1 - 1)
+            s = randint(0, min(2, abs(j - (i + 2))))
+
             move = Moves(randint(0, 2))
-            candidate_route: Route 
 
             self._iteration += 1
 
             if (i, j, move) in self._tabu_list:
                 continue
 
+            candidate_route = Route() 
+
+            # Pesquisa do custo
             match move:
                 case Moves.SWAP: 
-                    candidate_route = Swap(self.route, i, j)
+                    candidate_route._cost = SwapCalculateCost(self.route.copy(), i, j)
                 case Moves.TWOOPT: 
-                    candidate_route = TwoOPT(self.route, i, j)
+                    candidate_route._cost = TwoOPTCalculateCost(self.route.copy(), i, j)
                 case Moves.OROPT:
-                    # candidate_route = OrOPT(self.route, i, j, 1)
-                    # candidate_route = OrOPT(self.route, i, j, 2)
-                    # candidate_route = OrOPT(self.route, i, j, 3)
-                    candidate_route = OrOPT(self.route, i, j, randint(1, j - 1))
+                    candidate_route._cost = OrOPTCalculateCost(self.route.copy(), i, j, s)
             
-            if candidate_route.getCost() < self.route.getCost():
-                self.route = candidate_route
-                self._iteration = 0
+            if candidate_route.getCost() > self.route.getCost():
+                self._tabu_list.append(i, j, move)
                 continue
             
-            self._tabu_list.append(i, j, move)
+            # Mudanca de rota
+            match move:
+                case Moves.SWAP: 
+                    candidate_route._route = SwapCalculateRoute(self.route.copy(), i, j)
+                case Moves.TWOOPT: 
+                    candidate_route._route = TwoOPTCalculateRoute(self.route.copy(), i, j)
+                case Moves.OROPT:
+                    candidate_route._route = OrOPTCalculateRoute(self.route.copy(), i, j, s)
+
+            self.route._route = candidate_route._route
+            self._tabu_list = []
+            self._iteration = 0
         
         return self.route
     
     def isStop(self) -> bool:
         return self._iteration > self._max_iterations
-        return self.route.getCost() >= 
+        return self.route.getCost() <= self._lower_bound * 1.05
+    
+        if self.route.getCost() <= self._lower_bound * 1.05:
+            return self._iteration > self._max_iterations
+        
+        return False
 
 class Moves(Enum):
     SWAP = 0
