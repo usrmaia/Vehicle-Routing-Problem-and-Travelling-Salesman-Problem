f7c66dd0b5413d7ef234e5c1c83974c7e994f4b5
George Maia <georgemaiaf@gmail.com>
2023-08-09 13:40:46 -0300

    refactor

--- src/tabuSearch.py
+++ src/tabuSearch.py
@@ -10,30 +10,35 @@ class TabuSearch:
     def __init__(
         self,
         nodes: List[Node],
-        lower_bound: float,
+        intance_lower_bound: float,
         tabu_list_size: int,
         max_iterations: int,
     ):
         self._tabu_list = TabuList(tabu_list_size)
-        self._max_iterations = max_iterations
+
         self._iteration = 0
-        self._lower_bound = lower_bound
+        self._max_iteration = max_iterations
 
-        self.route: Route
+        self.intance_lower_bound = intance_lower_bound
 
-        # self.RandIns(nodes)
-        self.route = NearestNeighbor(nodes)
+        self.best_route: Route()
+        
+
+        # self.best_route = self.RandIns(nodes)
+        self.best_route = NearestNeighbor(nodes)
+
+        # free nodes
 
         self.tabuSearch()
 
-    def tabuSearch(self) -> Route:
-        while self.isStop():
-            i = randint(1, len(self.route) - 1 - 1 - 1)
-            j = randint(i + 1, len(self.route) - 1 - 1)
-            s = randint(0, min(2, j - i))
+    def tabuSearch(self):
+        while not self.isStop():
+            i = randint(1, len(self.best_route) - 1 - 1 - 1)
+            j = randint(i + 1, len(self.best_route) - 1 - 1)
+            segment = randint(0, min(2, j - i))
 
-            _heuristic = Heuristic(randint(0, 2))
-            move = Move(i, j, s, _heuristic)
+            heuristic = Heuristic(randint(0, 2))
+            move = Move(heuristic, i, j, segment)
 
             self._iteration += 1
 
@@ -43,50 +48,49 @@ class TabuSearch:
             candidate_route = Route()
 
             # Pesquisa do custo
-            match _heuristic:
+            match heuristic:
                 case Heuristic.SWAP:
-                    candidate_route._cost = SwapCalculateCost(self.route.copy(), i, j)
+                    candidate_route._cost = SwapCalculateCost(self.best_route.copy(), i, j)
                 case Heuristic.TWOOPT:
-                    candidate_route._cost = TwoOPTCalculateCost(self.route.copy(), i, j)
+                    candidate_route._cost = TwoOPTCalculateCost(self.best_route.copy(), i, j)
                 case Heuristic.OROPT:
                     candidate_route._cost = OrOPTCalculateCost(
-                        self.route.copy(), i, j, s
+                        self.best_route.copy(), i, j, segment
                     )
 
-            if candidate_route.getCost() > self.route.getCost():
+            if candidate_route.getCost() > self.best_route.getCost():
                 self._tabu_list.append(move)
                 continue
 
             # Mudanca de rota
-            match _heuristic:
+            match heuristic:
                 case Heuristic.SWAP:
-                    candidate_route._route = SwapCalculateRoute(self.route.copy(), i, j)
+                    candidate_route._route = SwapCalculateRoute(self.best_route.copy(), i, j)
                 case Heuristic.TWOOPT:
                     candidate_route._route = TwoOPTCalculateRoute(
-                        self.route.copy(), i, j
+                        self.best_route.copy(), i, j
                     )
                 case Heuristic.OROPT:
                     candidate_route._route = OrOPTCalculateRoute(
-                        self.route.copy(), i, j, s
+                        self.best_route.copy(), i, j, segment
                     )
 
-            self.route._route = candidate_route._route
-            self.route._cost = candidate_route.getCost()
+            self.best_route = candidate_route
             self._iteration = 0
 
-        return self.route
-
     def isStop(self) -> bool:
-        return self._iteration > self._max_iterations
-        return self.route.getCost() <= self._lower_bound * 1.05
+        return self._iteration > self._max_iteration
+        # return self.best_route.getCost() <= self._lower_bound * 1.05
 
-        if self.route.getCost() <= self._lower_bound * 1.05:
-            return self._iteration > self._max_iterations
+        # if self.best_route.getCost() <= self._lower_bound * 1.05:
+        #     return self._iteration > self._max_iterations
 
-        return False
+        # return False
 
     def getRoute(self) -> Route:
-        return self.route
+        self.best_route.calculateTotalCost()
+
+        return self.best_route
 
 
 class Heuristic(Enum):
@@ -95,11 +99,11 @@ class Heuristic(Enum):
     OROPT = 2
 
 class Move:
-    def __init__(self, i, j, s, h) -> None:
+    def __init__(self, heuristic, i, j, segment=None) -> None:
+        self.heuristic = heuristic
         self.i = i
         self.j = j
-        self.s = s
-        self.h = h
+        self.segment = segment
 
 
 class TabuList:
@@ -108,18 +112,19 @@ class TabuList:
         self._len = len
 
     def append(self, move: Move):
+        # Manutenção do tamanho da lista
         if len(self._tabu_list) > self._len:
             self._tabu_list.clear() # Abordagem que limpa a lista
             # self._tabu_list.pop(self._tabu_list.values()[0]) # Abordagem que remove o primeiro elemento
 
-        if move.h != Heuristic.OROPT:
-            move = (move.i, move.j, move.h)
+        if move.heuristic != Heuristic.OROPT:
+            move = Move(move.heuristic, move.i, move.j)
 
         self._tabu_list[move] = True 
         
 
     def __contains__(self, move: Move):
-        if move.h == Heuristic.OROPT:
+        if move.heuristic == Heuristic.OROPT:
             return move in self._tabu_list
         
-        return (move.i, move.j, move.h) in self._tabu_list
+        return (move.heuristic, move.i, move.j) in self._tabu_list
